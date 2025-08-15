7f140e7666803f8b1092dd6420df7a45484eeacb
nicolasfama <famanicolas@outlook.com>
2025-08-15 09:23:33 -0300

    primeros pasos

---
+++ sections/sections-2.ts
@@ -0,0 +1,19 @@
+// esto es para tener una idea del backup de los datos
+
+
+let numberOfLines= 0
+export function printnumberOfLines() {
+    numberOfLines= numberOfLines+ 1;
+    console.log("Linea # " + numberOfLines);
+
+}
+printnumberOfLines();
+printnumberOfLines(); 
+printnumberOfLines();
+printnumberOfLines();
+
+
+
+
+
+
